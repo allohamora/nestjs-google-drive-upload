7243f946e16a5117dec931baddeba341e4761539
allohamora <herman30698@gmail.com>
2025-03-08 12:48:36 +0200

    feat: add naming strategy

--- src/database/data-source.options.ts
+++ src/database/data-source.options.ts
@@ -1,14 +1,14 @@
 import { join } from 'node:path';
 import { config } from '../config/config.module';
 import { DataSourceOptions } from 'typeorm';
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { NamingStrategy } from './naming.strategy';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   url: config.POSTGRES_URL,
   entities: [join(__dirname, '..', '/**/*.entity{.ts,.js}')],
   migrations: [join(__dirname, '/migrations/*{.ts,.js}')],
-  namingStrategy: new SnakeNamingStrategy(),
+  namingStrategy: new NamingStrategy(),
   migrationsRun: true,
   logging: true,
   synchronize: false,
